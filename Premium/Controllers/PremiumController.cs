0c85d3b92e1fe059aa2712a6855d7306ebf9def2
Begüm <begumacaar@gmail.com>
2021-07-13 10:14:06 +0300

    13,07

---
+++ Premium/Controllers/PremiumController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Premium.Controllers
+{
+    public class PremiumController : Controller
+    {
+        // GET: Premium
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FeaturesPage()
+        {
+            return View();
+        }
+
+        public ActionResult ContactPage()
+        {
+            return View();
+        }
+    }
+}
